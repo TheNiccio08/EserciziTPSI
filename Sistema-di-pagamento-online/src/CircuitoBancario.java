fbf03dcfdbe94b33b8205b7bd4efd4bd86245f5f
Perego Nicolo <perego.nicolo@osdbsesto>
2026-01-23 14:07:37 +0100

    ADD: aggiunto classe CircuitoBancario e i suo metodi

--- Sistema-di-pagamento-online/src/CircuitoBancario.java
+++ Sistema-di-pagamento-online/src/CircuitoBancario.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
 public class CircuitoBancario {
+
+    private Random ran = new Random();
+
+    public boolean autorizzaPagamento(CartaDiCredito carta, double importo)
+            throws ConnessioneBancariaException {
+
+        int valore = ran.nextInt(101);
+
+        if (valore < 10) {
+
+            throw new ConnessioneBancariaException("Timeout di rete");
+
+        } else if (valore < 15) {
+
+            throw new FrodeRilevataException("Attività sospetta rilevata");
+
+        }
+        carta.scalaSaldo(importo);
+        return true;
+    }
 }
