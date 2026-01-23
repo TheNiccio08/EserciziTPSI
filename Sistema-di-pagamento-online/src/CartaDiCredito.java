4079d7086d3cb37ec871e9e9c0240c4c832fa332
Perego Nicolo <perego.nicolo@osdbsesto>
2026-01-23 14:00:30 +0100

    ADD: aggiunti variabili carta di credito costruttore e metodo isScaduto

--- Sistema-di-pagamento-online/src/CartaDiCredito.java
+++ Sistema-di-pagamento-online/src/CartaDiCredito.java
@@ -1,2 +1,31 @@
 public class CartaDiCredito {
+
+    private String numero;
+    private int scadenzaMese;
+    private int scadenzaAnno;
+    private String cvv;
+    private double saldo;
+
+    public CartaDiCredito(String numero, int scadenzaMese, int scadenzaAnno, String cvv, double saldo) {
+        this.numero = numero;
+        this.scadenzaMese = scadenzaMese;
+        this.scadenzaAnno = scadenzaAnno;
+        this.cvv = cvv;
+        this.saldo = saldo;
+    }
+
+    public boolean isScaduta() {
+        int mese = 1;
+        int anno = 2026;
+
+        if (scadenzaAnno < anno) {
+
+            return true;
+
+        } else if (scadenzaAnno == anno && scadenzaMese < mese){
+            return true;
+        }else{
+            return false
+        }
+    }
 }
